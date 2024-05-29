702c8ce2474228d334833668ecd92c74f56576a0
VastiDev <vastiane1@gmail.com>
2024-05-29 20:09:53 -0300

    configure test files

--- src/test/java/com/vastidev/parkingcontrol/ParkingControlApplicationTests.java
+++ src/test/java/com/vastidev/parkingcontrol/ParkingControlApplicationTests.java
@@ -2,8 +2,10 @@ package com.vastidev.parkingcontrol;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ParkingControlApplicationTests {
 
     @Test
